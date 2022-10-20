a5c167052a8afda4d1140993106416332870cb3a
Simone Pasquini <simo.pp07@gmail.com>
2022-10-20 17:34:17 +0200

    Update class

---
+++ ParticleType.cpp
@@ -0,0 +1,5 @@
+#include "ParticleType.hpp"
+
+auto ParticleType::GetName() const { return fName; }
+auto ParticleType::GetMass() const { return fMass; }
+auto ParticleType::GetCharge() const { return fCharge; }
--- ParticleType.hpp
+++ ParticleType.hpp
@@ -1,5 +1,27 @@
 #ifndef PARTICLETYPE_HPP
 #define PARTICLETYPE_HPP
 
+#include <iostream>
+
+class ParticleType {
+ public:
+  auto GetName() const;
+  auto GetMass() const;
+  auto GetCharge() const;
+
+  ParticleType(char* name = '\0', double mass = 0., int charge = 0)
+      : fName{name}, fMass{mass}, fCharge{charge} {}
+
+ private:
+  char* const fName;
+  double const fMass;
+  int const fCharge;
+};
+
+inline std::ostream& operator<<(std::ostream& os, ParticleType const& p) {
+  os << "Name: " << p.GetName() << "Mass: " << p.GetMass()
+     << "Charge: " << p.GetCharge();
+  return os;
+}
 
 #endif
